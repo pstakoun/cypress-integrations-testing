d20f11f9611b9e6144c92aa003f923444d9e261f
Peter Stakoun <peter@stakoun.com>
2022-12-07 15:21:24 -0500

    Modify and add tests

--- cypress/e2e/spec6.cy.js
+++ cypress/e2e/spec6.cy.js
@@ -1,7 +1,7 @@
 describe('empty spec', () => {
-  it('another', function() {
+  it('cra', function() {
     cy.visit('http://localhost:3000');
-    cy.get('p').should('have.text', 'blah');
+    cy.get('p').should('have.text', 'edit src/app.js and save to reload.');
     cy.get('.app-link').should('have.text', 'learn react');
   });
   it('cra', function() {
@@ -17,16 +17,16 @@ describe('empty spec', () => {
   it('cra', function() {
     cy.visit('http://localhost:3000');
     cy.get('p').should('have.text', 'Edit src/App.js and save to reload.');
-    cy.get('.App-link').should('have.text', 'oops');
-  });
-  it('more failed', function() {
-    cy.visit('http://localhost:3000');
-    cy.get('p').should('have.text', 'src/App.js and save to reload.');
     cy.get('.App-link').should('have.text', 'Learn React');
   });
   it('cra', function() {
     cy.visit('http://localhost:3000');
     cy.get('p').should('have.text', 'Edit src/App.js and save to reload.');
+    cy.get('.App-link').should('have.text', 'Learn React');
+  });
+  it('yay another one', function() {
+    cy.visit('http://localhost:3000');
+    cy.get('p').should('have.text', 'Edit and save to reload.');
     cy.get('.App-link').should('have.text', 'Learn React');
   });
   it('cra', function() {
